53d08b0e78d3d991d75a8d1db3ebed017a2fc509
Zakarya <52080782+ZakaryaBoudraf@users.noreply.github.com>
2024-11-27 17:04:35 +0100

    Green Phase 2 - US5

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -39,15 +39,6 @@ class TestIntelligentOffice(unittest.TestCase):
 
     @patch.object(VEML7700, "lux", new_callable=PropertyMock)
     @patch.object(GPIO, "output")
-    def test_manage_light_level_turn_on_light_if_lower_than_500_lux(self, mock_lightbulb: Mock, mock_ambient_light_sensor: Mock):
-        mock_ambient_light_sensor.return_value = 499
-        system = IntelligentOffice()
-        system.manage_light_level()
-        mock_lightbulb.assert_called_with(system.LED_PIN, True)
-        self.assertTrue(system.light_on)
-
-    @patch.object(VEML7700, "lux", new_callable=PropertyMock)
-    @patch.object(GPIO, "output")
     def test_manage_light_level_turn_off_light_if_higher_than_550_lux(self, mock_lightbulb: Mock, mock_ambient_light_sensor: Mock):
         mock_ambient_light_sensor.return_value = 551
         system = IntelligentOffice()
